bf4e4f2bcdd5a6c9da5160b3a86b82d12d2e44d7
Mohammad Samiul Islam <forthright48@gmail.com>
2018-08-03 11:39:23 +0800

    Initial commit

---
+++ config/index.js
@@ -0,0 +1,15 @@
+module.exports = {
+    secretModule: require('../secret.js'),
+    port: process.env.PORT || 8002,
+    // database: {
+    //   init() {
+    //     require('./database.js');
+    //   }
+    // },
+    // session: {
+    //   init(app) {
+    //     require('./session.js').addSession(app);
+    //   }
+    // }
+  };
+
---
+++ index.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const config = require('config');
+const logger = require('custom_modules').logger;
+const morgan = require('morgan');
+
+const app = express();
+const server = require('http').createServer(app);
+
+app.set('port', config.port);
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({
+  extended: true,
+})); // support encoded bodies
+
+// config.database.init();
+// config.session.init(app);
+
+// /* Models*/
+// require('./models/userModel.js');
+// require('./models/gateModel.js');
+// require('./models/problemBankModel.js');
+
+/* Middlewares */
+app.use(morgan('dev'));
+
+/* API Public*/
+
+// require('./api/public/auth.js').addRouter(app);
+
+// require('./api/v1/users.js').addRouter(app);
+// require('./api/v1/ojInfo.js').addRouter(app);
+// require('./api/v1/gateway.js').addRouter(app);
+// require('./api/v1/problemBank.js').addRouter(app);
+
+/* Error Handling */
+app.use('/api/', function(err, req, res, next) {
+  if (process.env.NODE_ENV === 'dev') {
+    console.log(err);
+  }
+  const status = err.status || 500;
+  if ( status == 500 ) {
+    logger.error(err.stack);
+  }
+  return res.status(status).json({
+    status,
+    message: err.message,
+    error: process.env.NODE_ENV === 'dev'? err : undefined,
+  });
+});
+
+process.on('unhandledRejection', (error) => {
+  logger.error({
+    severe: true,
+    error: error.stack,
+  });
+  process.exit(1);
+});
+
+process.on('uncaughtException', function(error) {
+  logger.error({
+    severe: true,
+    error: error.stack,
+  });
+  process.exit(1);
+});
+
+if (require.main === module) {
+  server.listen(app.get('port'), function() {
+    logger.info(`Server running at port ${app.get('port')}`);
+  });
+} else {
+  module.exports = {
+    server,
+    app,
+  };
+}
