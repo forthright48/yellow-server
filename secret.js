9beeb11fc1c785e46e17d1ce6e402c200e934b12
Mohammad Samiul Islam <forthright48@gmail.com>
2018-08-03 17:06:31 +0800

    Add secret info

---
+++ secret.js
@@ -0,0 +1,13 @@
+module.exports = {
+    secret: 'garenahackathonyellowsubmarine',
+    dburl: 'mongodb://yellowdb:27017/yellow',
+    // mailApi: "secret-mail-api",
+    // siteName: "cpps",
+    // domain: "localhost:8000",
+    // subdomain: "www",
+    // recaptcha: {
+    //   site: "some-site",
+    //   secret: "some-secret"
+    // },
+  }
+;
